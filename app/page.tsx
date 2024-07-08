05ca99e4978939a8ec6f363de34b0ec988546e5d
tlair <lair.thomas2010@gmail.com>
2024-07-08 19:44:14 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,112 @@
-import Image from "next/image";
+'use client'
+
+import {useEffect, useRef} from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const menuRef = useRef<HTMLElement>()
+    const titles = useRef<string[]>([])
+
+    useEffect(() => {
+        titles.current = Array.from(menuRef.current.children)
+            .map(titleElement => titleElement.innerHTML);
+
+        window.addEventListener("scroll", function () {
+            const screenHeight = window.innerHeight;
+            const limit = 50;
+            const scrollThreshold = limit + 100 /* menu.bottom */ + 79 /* title element height */;
+
+            const menu = menuRef.current;
+            const menuPosition = menu.getBoundingClientRect().top;
+
+            if (menuPosition < limit && !menu.classList.contains("fixed")) {
+                menu.classList.add('fixed');
+            } else if (menu.classList.contains("fixed") && this.scrollY < screenHeight - scrollThreshold) {
+                menu.classList.remove('fixed');
+            }
+
+            const activeSectionElement = document.elementFromPoint(10, menuPosition + 79);
+            const activeSection = activeSectionElement.classList[1] || activeSectionElement.parentElement.classList[1] || activeSectionElement.parentElement.parentElement.classList[1]
+            const activeSectionIndex = titles.current.findIndex((e) => e === activeSection);
+            const activeSectionTitle = titles.current[activeSectionIndex];
+
+            Array.from(menuRef.current.children)
+                .forEach((titleElement) => {
+                    if (titleElement.innerHTML === activeSectionTitle) {
+                        titleElement.classList.add("active")
+                    } else {
+                        titleElement.classList.remove("active");
+                    }
+                })
+        })
+
+
+    }, []);
+
+    return (
+        <div>
+            <div className="background"></div>
+
+            <div className="main-title">CHARLOTTE DAUPHIN</div>
+            <div id="menu" ref={menuRef} className="menu">
+                <div className="menu-title"
+                     onClick={() => window.location.href = 'https://charlottedauphin.world/pages/humann-prize'}>STUDIO
+                </div>
+                <div className="menu-title"
+                     onClick={() => window.location.href = 'https://www.imdb.com/title/tt30268321'}>PRODUCTIONS
+                </div>
+                <div className="menu-title"
+                     onClick={() => window.location.href = 'https://dauphin.paris/fr/pages/e-space'}>SPACES
+                </div>
+                <div className="menu-title">FONDATION</div>
+            </div>
+
+            <div className="section STUDIO">
+                <img src="/background/bg-studio.jpg"
+                     onClick={() => window.location.href = 'https://charlottedauphin.world/pages/humann-prize'}/>
+            </div>
+            <div className="section PRODUCTIONS">
+                <div className="block-title">ELEANOR THE GREAT</div>
+                <div className="block-text">
+                    <div>AFTER SEVENTY YEARS WITH BEST FRIEND, ELEANOR MOVES TO NEW YORK CITY FOR A FRESH START. MAKING
+                        NEW
+                        FRIENDS
+                        AT NINETY PROVES DIFFICULT. LONGING FOR CONNECTION, SHE BEFRIENDS 19-YEAR-OLD,
+                    </div>
+
+                    <div> LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. MAECENAS EU AUCTOR SAPIEN, NON
+                        FINIBUS
+                        LIGULA. UT
+                        ULTRICIES VITAE LACUS AC PHARETRA. CURABITUR MAXIMUS MASSA FELIS, ULLAMCORPER CONSEQUAT JUSTO
+                        MAXIMUS AT.
+                        MORBI NISI TELLUS, FACILISIS SIT AMET ANTE A, VENENATIS PULVINAR NULLA, ALIQUAM BIBENDUM DOLOR
+                        NON FELIS
+                        SODALES ELEIFEND. DONEC EU LECTUS EST. SUSPENDISSE ALIQUAM LOREM EU MASSA TEMPOR, QUIS EFFICITUR
+                        ENIM
+                        TRISTIQUE. AENEAN DICTUM NISI SED ELEMENTUM VARIUS. AENEAN ET PHARETRA IPSUM. MAURIS SED ERAT
+                        SAGITTIS,
+                        SEMPER METUS AT, POSUERE QUAM. SUSPENDISSE SIT AMET SUSCIPIT ERAT. PRAESENT LUCTUS LIBERO AC
+                        LACUS
+                        ALIQUET,
+                        ID VARIUS ELIT SCELERISQUE. MORBI ET MAXIMUS URNA. FUSCE TRISTIQUE IPSUM IN LIGULA SAGITTIS,
+                        EGET
+                        PORTTITOR
+                        AUGUE ULTRICIES. DUIS LOBORTIS, LOREM NEC IMPERDIET PULVINAR, LIBERO ANTE CONVALLIS MAGNA,
+                        VOLUTPAT
+                        CONSEQUAT ORCI NULLA ET NUNC.
+                    </div>
+
+                    <div className="block-line"><b>DIRECTOR</b> SCARLETT JOHANSSON</div>
+                    <div className="block-line"><b>WRITER</b> TORY KAMEN</div>
+                    <div className="block-line"><b>STARS</b> CHIWETEL EJIOFOR, JUNE SQUIBB, JESSICA HECHT</div>
+                    <div className="block-line"><b>PRODUCER</b> CHARLOTTE DAUPHIN</div>
+
+                </div>
+                <img src="/background/bg-prod.png"/>
+
+            </div>
+            <div className="section SPACES"><img src="/background/placeholder.jpg"/></div>
+            <div className="section FONDATION"><img src="/background/placeholder.jpg"/></div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
