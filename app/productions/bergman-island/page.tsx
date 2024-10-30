c32e710ad7a9ff9a3fad7a91d4397d54a22b8da3
tlair <lair.thomas2010@gmail.com>
2024-10-30 20:46:46 +0100

    init

--- app/productions/bergman-island/page.tsx
+++ app/productions/bergman-island/page.tsx
@@ -66,6 +66,77 @@ export default function BergmanIsland() {
                                 </div>
                             </div>
 
+                            <div onClick={() => {
+                                document.getElementById("moreInfos").style.display = "flex";
+                                document.getElementById("moreInfosToggle").style.display = "none";
+                                document.getElementsByClassName("production-lines")[0].style.display = "none";
+                            }} id="moreInfosToggle" className="more-infos">
+                                <div>More infos <div className="more-infos-square"></div></div>
+                            </div>
+                            <div id="moreInfos" className="more-infos-content">
+                                <div className="more-infos-content-line">
+                                    <div>Titre</div>
+                                    <div>Bergman Island</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Réalisation et scénario</div>
+                                    <div>Mia Hansen-Løve</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Photographie</div>
+                                    <div>Denis Lenoir</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Montage</div>
+                                    <div>Marion Monnier</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Décors</div>
+                                    <div>Béatrice Strand</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Costumes</div>
+                                    <div>Judith de Luze</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Musique</div>
+                                    <div>Raphaël Hamburger</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Son</div>
+                                    <div>Paul Heymans</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Production</div>
+                                    <div>Marc Brégain, Julie Viez</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Sociétés de production</div>
+                                    <div>CG Cinéma, Arte France Cinéma et Barnstormer Productions</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Société de distribution</div>
+                                    <div>Les Films du losange</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Pays de production</div>
+                                    <div>France - Belgique - Allemagne - Suède</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Langue originale</div>
+                                    <div>Anglais</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Durée</div>
+                                    <div>112 minutes</div>
+                                </div>
+                                <div className="more-infos-content-line">
+                                    <div>Dates de sortie</div>
+                                    <div>juillet 2021 (Festival de Cannes 2021)</div>
+                                </div>
+
+                            </div>
+
                             <div className="production-main-image">
                                 <img src="/assets/production/bergman/affiche.webp"/>
                             </div>
